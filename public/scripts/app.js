83e0232c66f0a50aaebfd89c4dedbefd0e1830d7
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-21 20:52:18 +0000

    started click toggle functionality for orders

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,7 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$(document).ready(() => {
+
+  // Order toggle
+  $('.order-head').click(() => {
+    console.log('Click!');
+  });
 });
