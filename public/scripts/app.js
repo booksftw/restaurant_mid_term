0495a3d9045a59ab58ee98d3e140cf3790985ccc
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-21 23:33:17 +0000

    created toggle script to drop down order items

--- public/scripts/app.js
+++
@@ -1,9 +0,0 @@
-const $jq = jQuery.noConflict();
-
-$jq(document).ready(() => {
-
-  // Order toggle
-  $jq('.order-head').click(() => {
-    $jq('.order-head').siblings().slideToggle('fast');
-  });
-});
