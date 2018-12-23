540b35db4ad5b27cc3c83b7ab7d4236ebe85823d
Jimyung Park <pzy51992@hotmail.com>
2018-12-23 02:03:50 +0000

    updated comment

---
+++ menu_items.js
@@ -0,0 +1,22 @@
+const dishesTable = [
+{
+  id: 1,
+  name: "Gio's Hearty Breakfas",
+  description: "Two eggs any style, your choice of bacon, back bacon, sausage or chorizo, & your choice of toast, buttermilk pancakes or a half waffle. Served with endless smashbrown potatoes.",
+  image_url: "https://cdad5c1a4432622c031d-9e2498b30009308de15a0d52a7422974.ssl.cf1.rackcdn.com/menu/product/hearty-nine-grain-pancake-breakfast_thumbnaillarge_2018-04-02-03-08-00.jpg",
+  price: 18,
+},
+{
+  id: 2,
+  name: "Breakfast Quesadilla",
+  description: "Mushrooms, fresh spinach, three cheeses, green onions & scrambled eggs. Served with cilantro sour cream & strawberry pico de gallo. Add bacon or chorizo sausage",
+  image_url: "http://cdn1.thegourmetbachelor.com/wp-content/uploads/2010/12/TGB_Breakfast_Quesadilla1.jpg",
+  price: 15,
+},
+{
+  id: 3,
+  name: "Oatmeal & Quinoa",
+  description: "Sundried cranberries & toasted almonds. Served with fresh fruit.",
+  image_url: "https://foodal.com/wp-content/uploads/2017/10/The-Best-Hot-Breakfast-Cereal-with-Strawberries-and-Nuts.jpg",
+  price: 11,
+}];
