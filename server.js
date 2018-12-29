47d891effa1ae752145a2b39eaaf3550f506641a
Nicholas Zachary <nicholas.zachary@outlook.com>
2018-12-29 22:17:26 +0000

    switch branch

--- server.js
+++ server.js
@@ -53,32 +53,25 @@ app.use("/api/users", usersRoutes(knex));
 
 
 /** 
- * ~ Passport Authentication 
+ * ~ Custom Authentication 
  */
 
 // ~ Where I left off: http://www.passportjs.org/packages/passport-local/ . The passport strategy requires a verify callback. 
 
-var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-    
-var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
-opts.issuer = 'accounts.examplesoft.com';
-opts.audience = 'yoursite.net';
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
-    });
-}));
+app.use( function (req,res,next) {
+  console.log('Time' , Date.now())
+
+  // * call a function to check authentication
+  // STORE AND CHECK COOKIE TO AUTHENTICATED
+  const userAuthenticated = true;
+  
+  // User not authenticated redirect to login/register page
+  // User authenticated
+    // show visited route
+  userAuthenticated ? next() : res.send('login')
+
+
+})
 
 
 // ~ END OF PASPORT
